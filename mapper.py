5b547ba08ed59e85aa570477585c2dacd8bd4d0f
Lucas Ferreira <lucas0409lf@gmail.com>
2021-11-19 15:35:15 -0300

    parte 2

--- mapper.py
+++ mapper.py
@@ -1,9 +1,18 @@
+#!/usr/bin/python3
 import sys
+
+
 # index,matricula,nome,data_contratacao,status,time
-for line in sys.stdin:
-    line = line.strip()
-    index, matricula, nome, data_contratacao, status, time = line.split(',')
 
-    ano, mes, _ = data_contratacao.split('-')
+def main():
+    for line in sys.stdin:
+        line = line.strip()
+        index, matricula, nome, data_contratacao, status, time = line.split(',')
 
-    print(f'{nome},{ano},{mes},{status},{time}')
+        ano, mes, _ = data_contratacao.split('-')
+
+        print(f'{nome},{ano},{mes},{status},{time}')
+
+
+if __name__ == '__main__':
+    main()
--- reducer.py
+++ reducer.py
@@ -1,18 +1,25 @@
+#!/usr/bin/python3
+
 from collections import defaultdict
 import sys
 
 
-dict_anos = defaultdict(list)
+def main():
+    # saber quantos funcionarios foram contratados em cada ano
+    dict_anos = defaultdict(list)
 
-for line in sys.stdin:
-    line = line.strip()
+    for line in sys.stdin:
+        line = line.strip()
 
-    nome, ano, mes, status, time = line.split(',')
+        nome, ano, mes, status, time = line.split(',')
 
-    dict_anos[ano].append(f'{nome},{status},{time}')
+        dict_anos[ano].append(f'{nome},{status},{time}')
 
-dict_anos = sorted(dict_anos.items())
+    dict_anos = sorted(dict_anos.items())
+
+    for k, v in dict(dict_anos).items():
+        print(f'{k},{len(v)}')
 
 
-for k,v in dict(dict_anos).items():
-    print(f'{k},{len(v)}')
+if __name__ == '__main__':
+    main()
